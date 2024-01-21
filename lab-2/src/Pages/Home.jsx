17f9ab814524e4ef61872fdf1c978b9932458a5c
MemmedovaEsmira <memmedovaesmira151@gmail.com>
2024-01-21 23:10:53 +0400

    lab 16 added

--- lab-2/src/Pages/Home.jsx
+++ lab-2/src/Pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Slider from '../components/Slider/Slider'
 
+import img1 from "../../src/assets/images/img_1.jpg"
+import img2 from "../../src/assets/images/img_2.jpg"
+import img3 from "../../src/assets/images/img_3.jpg"
 
-const Homepages = () => {
+const Home = () => {
   return (
     <>
       <Slider/>
+      <img src={img1} alt="image" />
+     <img src={img2} alt='image' />
+     <img src={img3} alt='image' />
     </>
   )
 }
 
-export default Homepages
+export default Home
