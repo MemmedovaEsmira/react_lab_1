847ee8c33fd5f9c3809e5ff3966b99d79df2cb0e
MemmedovaEsmira <memmedovaesmira151@gmail.com>
2024-01-23 19:10:22 +0400

    lab added

--- lab-2/src/Pages/Home.jsx
+++ lab-2/src/Pages/Home.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react';
 import Slider from '../components/Slider/Slider'
+import img1 from "../assets/Images/img_1.jpg"
 
-import img1 from "../../src/assets/images/img_1.jpg"
-import img2 from "../../src/assets/images/img_2.jpg"
-import img3 from "../../src/assets/images/img_3.jpg"
+import img2 from "../assets/Images/img_2.jpg"
+import img3 from "../assets/Images/img_3.jpg"
 
-const Home = () => {
-  return (
-    <>
-      <Slider/>
-      <img src={img1} alt="image" />
-     <img src={img2} alt='image' />
-     <img src={img3} alt='image' />
-    </>
-  )
+
+function  Home  ()  {
+  const Images=[
+    "img1",
+    "img2",
+    "img3",
+  ]
+ 
+
+     return <Slider
+     img1={img1}
+     img2={img2}
+     img3={img3}
+      />
+   
 }
 
-export default Home
+export default Home;
--- lab-2/src/components/Slider/Slider.jsx
+++ lab-2/src/components/Slider/Slider.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { GrChapterNext } from "react-icons/gr";
 import { GrChapterPrevious } from "react-icons/gr";
 import "./slider.css";
+// import img1 from "../../assets/Images/img_1.jpg"
 
+import img2 from "../assets/Images/img_2.jpg"
+import img3 from "../assets/Images/img_3.jpg"
 
 const Slider = (props) => {
   return (
     <div >
+      <div className='images'>
+
       <GrChapterNext />
       <GrChapterPrevious />
-
+      {/* {props.img1} */}
+      {img2}
+      {img3}
+      </div>
+        
     </div>
   )
 }
